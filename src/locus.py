29fc525a224a3ad8152d4cedef98dcb470cc5814
Gerald McCollam <gerald.mccollam@gmail.com>
2017-02-13 21:49:18 -0600

    cleans up Locus

--- src/locus.py
+++ src/locus.py
@@ -25,19 +25,16 @@ class Locus(object):
             self.alleles.append(allele)
 
     def add_allele_list(self, allele_list=[]):
-        if len(self.alleles) > c.ALLELE_RANGE[1]-1:
-            raise ValueError('Number of alleles per locus has been exceeded.')
         if ( len(allele_list) + len(self.alleles) > c.ALLELE_RANGE[1]-1):
             raise ValueError('Number of alleles per locus has been exceeded.')
 
         self.alleles = self.alleles + allele_list
 
+    def get_random_allele(self):
+        return random.choice(self.alleles) 
+
     def __iter__(self):
         return iter(self.alleles)
-
-    def __getslice__(self, a, b):
-        """ Returns a slice of alleles list """
-        return self.alleles[a:b]
 
     def __repr__(self):
         return '{:s}: {:s}'.format(self.name,
