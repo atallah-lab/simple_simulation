14b151547428d169b9859ecd0b9941ee1c8b17ac
Gerald McCollam <gerald.mccollam@gmail.com>
2017-02-22 16:44:40 -0600

    prints allele freq. over generations

--- main.py
+++ main.py
@@ -3,12 +3,8 @@
 # -*- coding: utf-8 -*-
 
 import random
+import unittest
 import os
-import collections
-import math
-import numpy as np      # for array manipulation
-import time             # estimate runtime
-import copy
 
 import src.constants as c
 from src.allele import Allele
@@ -16,6 +12,34 @@ from src.locus import Locus
 from src.individual import Individual
 from src.population import Population
 
+def summarize_alleles_per_generation(p):
+    generations = 100
+    original_population_size = c.MAX_POPULATION
+    remaining_population_size = 0
+    #print ('length at begining of generation {:d} is {:d}'.format(generation+1, len(p.individuals)))
+
+    new_individuals = []
+    for generation in range(0, generations):
+        for individual in p.individuals:
+            if random.random() > float(individual.get_fitness()):
+                p.individuals.remove(individual)
+
+        diff = original_population_size-len(p.individuals)
+        remaining_population_size = original_population_size - diff
+
+        for i in range(diff):
+            random_alleles = []
+            for j in range(c.ALLELE_RANGE[0], c.ALLELE_RANGE[1]):
+                random_alleles.append(p.get_random_allele_from_population())
+            p.add_new_individual_to_population(Individual(random_alleles))
+
+        print ('For generation {:d} allele frequency is:'.format(generation+1))
+        #p.summarize_population()
+
+        for allele in ['A', 'a', 'T', 't', 'C', 'c', 'G', 'g']:
+            print (allele, p.get_allele_frequency(allele))
 
 if __name__ == '__main__':
-    pass
+
+    p = Population()
+    summarize_alleles_per_generation(p)
