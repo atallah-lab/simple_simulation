5b1d5b63fa8909d157e29a85c24555f9878a1e12
Gerald McCollam <gerald.mccollam@gmail.com>
2017-02-22 15:56:51 -0600

    moves code to tests/test_advanced.py

--- main.py
+++ main.py
@@ -18,28 +18,4 @@ from src.population import Population
 
 
 if __name__ == '__main__':
-
-    generations = 10
-    original_population_size = c.MAX_POPULATION
-    remaining_population_size = 0
-
-    p = Population()
-    new_individuals = []
-
-    for generation in range(0, generations):
-        print ('length at begining of interation {:d} is {:d}'.format(generation, len(p.individuals)))
-
-        for individual in p.individuals:
-            if random.random() > float(individual.get_fitness()):
-                p.individuals.remove(individual)
-
-        diff = original_population_size-len(p.individuals)
-        remaining_population_size = original_population_size - diff
-
-        print ('length after culling in gen {:d} is {:d}'.format(generation, len(p.individuals)))
-
-        for i in range(diff):
-            random_alleles = []
-            for j in range(c.ALLELE_RANGE[0], c.ALLELE_RANGE[1]):
-                random_alleles.append(p.get_random_allele_from_population())
-            p.add_new_individual_to_population(Individual(random_alleles))
+    pass
